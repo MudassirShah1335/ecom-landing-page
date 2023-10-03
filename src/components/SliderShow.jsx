92d6a0d2d95ec16fbe5f93383658622af5a809d5
unknown <abdulmajid1m2@gmail.com>
2023-10-03 09:37:58 +0500

    depoy to firebase

--- src/components/SliderShow.jsx
+++ src/components/SliderShow.jsx
@@ -1,34 +1,32 @@
 import React from "react";
+import logo from "../Images/logo.jpg";
 
 function SliderShow() {
   return (
     <div className="m-10 mx-auto p-16 sm:p-24 lg:p-48 bg-gray-200">
       <div className="relative rounded-lg block md:flex items-center bg-gray-100 shadow-xl min-h-72">
-        <div className="relative w-full md:w-2/5 h-full overflow-hidden rounded-t-lg md:rounded-t-none md:rounded-l-lg min-h-72">
+        {/* Logo on the left side */}
+        <div className="w-1/5 md:w-2/5 h-full overflow-hidden rounded-t-lg md:rounded-t-none md:rounded-l-lg min-h-72"
+          style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+        >
           <img
-            className="absolute inset-0 w-full h-full object-cover object-center"
-            src="https://stripe.com/img/v3/payments/overview/photos/missguided.jpg"
-            alt=""
+            className=""
+            style={{ width: "200px", height: "200px", objectFit: "contain" }}
+            src={logo} // Use the imported logo here
+            alt="Logo"
           />
-          <div className="absolute inset-0 w-full h-full bg-indigo-900 opacity-75"></div>
-          <div className="absolute inset-0 w-full h-full flex items-center justify-center fill-current text-white">
-            <svg
-              className="w-full h-24"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 239 120"
-            >
-              {/* Your SVG path goes here */}
-            </svg>
-          </div>
         </div>
+        {/* Rest of your content */}
         <div className="w-full md:w-3/5 h-full flex items-center bg-gray-100 rounded-lg">
           <div className="p-12 md:pr-24 md:pl-16 md:py-12">
             <p className="text-gray-600">
-              <span className="text-gray-900">Missguided</span> is a UK-based
-              fashion retailer that has nearly doubled in size since last year.
-              They integrated Stripe to deliver seamless checkout across mobile
-              and web for customers in 100+ countries, all while automatically
-              combating fraud.
+              <span className="text-gray-900">
+                Attrack Game: Elevate Your Gaming Experience
+              </span>{" "}
+              Join the ranks of satisfied gamers worldwide with Attrack Game.
+              Our cutting-edge platform delivers thrilling gaming adventures
+              while ensuring seamless, secure transactions for players across
+              the globe. Get ready to level up your gaming journey today!
             </p>
             <a
               className="flex items-baseline mt-3 text-indigo-600 hover:text-indigo-900 focus:text-indigo-900"
@@ -46,6 +44,7 @@ function SliderShow() {
             <polygon points="50,0 100,0 50,100 0,100" />
           </svg>
         </div>
+        {/* Buttons and other content */}
         <button className="absolute top-0 mt-32 left-0 bg-white rounded-full shadow-md h-12 w-12 text-2xl text-indigo-600 hover:text-indigo-400 focus:text-indigo-400 -ml-6 focus:outline-none focus:shadow-outline">
           <span className="block" style={{ transform: "scale(-1)" }}>
             &#x279c;
@@ -57,7 +56,7 @@ function SliderShow() {
           </span>
         </button>
       </div>
-
+      {/* Additional content below the hero section */}
       <div className="flex items-center pt-5 justify-between">
         <button className="px-2 opacity-50 hover:opacity-100 focus:opacity-100">
           <img
@@ -100,7 +99,8 @@ function SliderShow() {
           />
         </button>
       </div>
-    </div>
+    </div >
   );
 }
+
 export default SliderShow;
