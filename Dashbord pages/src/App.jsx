20f7ca8d6e32aab8385f2f02b9fcb00d59ac2ee6
MudassirShah1335 <MudassirShah1335@gmail.com>
2023-10-18 23:56:02 +0500

    term plarfom changed

---
+++ Dashbord pages/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AccoutInfo from "./pages/AccoutInfo";
+
+const App = () => {
+  return (
+    <div>
+      <AccoutInfo />
+    </div>
+  );
+};
+
+export default App;
---
+++ Dashbord pages/src/pages/AccoutInfo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../MainLayots/Header";
+import Footer from "../MainLayots/Footer";
+const AccoutInfo = () => {
+  return (
+    <>
+      <Header />
+      <section></section>
+      <Footer />
+    </>
+  );
+};
+
+export default AccoutInfo;
