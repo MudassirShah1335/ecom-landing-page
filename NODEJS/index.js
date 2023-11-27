ac1a337c882ef4e86ab06b670dfe0c348b3f6dfa
MudassirShah1335 <MudassirShah1335@gmail.com>
2023-11-27 18:44:19 +0500

    Githubtutorial

--- NODEJS/index.js
+++
@@ -1,6 +0,0 @@
-// const check = "mudassri shah";
-// console.log(check);
-
-const fs = require("fs");
-fs.writeFileSync("read.txt", "wellcome to node js");
-fs.appendFile("read.txt", "wow amazing");
